be9d4579771e40aa8aa3753a347150e9a10fe551
Christopher France <hipsterelitist@gmail.com>
2013-04-17 10:05:06 -0400

    add visual asset manipulation class to gem

---
+++ lib/playapi/visuals.rb
@@ -0,0 +1,32 @@
+require 'playapi/utils'
+
+module Playapi
+	class Visual
+		extend Playapi::Utils
+
+		class << self
+
+			def get_all
+				url "api/v2/visuals"
+				get_object(:get, "visuals", url)
+			end
+
+			def get(id)
+				url = "api/v2/visuals/#{id}"
+				get_object(:get, "visual", url)
+			end
+
+			def create(opts)
+				url = "api/v2/visuals"
+				get_object(:post, "visual", url, opts)
+			end
+
+			def update(id, opts)
+				url = "api/v2/visuals/#{id}"
+				get_object(:put, "visual", url, opts)
+			end
+
+		end
+
+	end
+end
