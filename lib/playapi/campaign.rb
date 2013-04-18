4fe761527476ae9ca1b3a13c6e4f911b463f0664
Christopher France <hipsterelitist@gmail.com>
2013-04-18 12:34:44 -0400

    some documentation and interaction sorts for interaction and campaign

--- lib/playapi/campaign.rb
+++ lib/playapi/campaign.rb
@@ -8,18 +8,36 @@ module Playapi
 
 		class << self
 
+			# Get the primary campaign for which you are currently authorized, one can optionally 
+			# pass the name of the campaign, if your credentials are good for multiple campaigns
+
 			def get(campaign_name = "index")
 				url = "api/v2/campaigns/"
 				#url += "/#{campaign_name}" unless campaign_name == nil
 				get_object(:get, "campaign", url)
 			end
 
-			def get_interactions(campaign_name)
-				url = "api/v2/campaigns/#{campaign_name}"
-				get_object(:get, "campaign", url).interactions
+			# Get interactions for a campaign, optional paramaters include
+			# Optional params hash (opts)
+			# :sort=STRING 		(Valid options are: leaders, newest, oldest)
+			# :approved=BOOL	(Show only approved interactions)
+			# :pending=BOOL		(Show only pending (unapproved) interactions)
+			# :limit=INTEGER 	(Limit to a number of entries, defaults to 10)
+			# :page=INTEGER		(Determines the offset for limit, defaults to 1)
+			# :type=STRING 		(Valid options are Instapic, Tweet, Checkin)
+			def get_interactions(campaign_name, opts = {})
+				#* This could just called Playapi::Interaction.list....
+				url = "api/v2/campaigns/#{campaign_name}/interactions"
+				get_object(:get, "interactions", url, opts).interactions
 			end
 
+			# Get interactions with the most points (highest score) for a campaign
+			# Optional params hash (opts)
+			# :limit=INTEGER 	(Limit to a number of entries, defaults to 10)
+			# :page=INTEGER		(Determines the offset for limit, defaults to 1)
+			# :type=STRING 		(Valid options are Instapic, Tweet, Checkin)
 			def leaders(campaign_name, opts = {})
+				#* This could just call Playapi::Interactions.list with most points params...
 				url = "api/v2/campaigns/#{campaign_name}/leaders"
 				get_object(:get, "results", url, opts)
 			end
--- lib/playapi/interaction.rb
+++ lib/playapi/interaction.rb
@@ -7,16 +7,32 @@ module Playapi
 
 		class << self
 
+			# Get interactions for a campaign, optional paramaters include
+			# Optional params hash (opts)
+			# :sort=STRING 		(Valid options are: leaders, newest, oldest)
+			# :approved=BOOL	(Show only approved interactions)
+			# :pending=BOOL		(Show only pending (unapproved) interactions)
+			# :limit=INTEGER 	(Limit to a number of entries, defaults to 10)
+			# :page=INTEGER		(Determines the offset for limit, defaults to 1)
+			# :type=STRING 		(Valid options are Instapic, Tweet, Checkin)
+			def list
+				url = "api/v2/interactions"
+				get_object(:get, "interactions", url)
+			end
+
+			# Get an interaction with the given id
 			def get(id)
 				url = "api/v2/interactions/#{id}"
 				get_object(:get, "interaction", url)
 			end
 
+			# Create a custom interaction for your campaign
 			def create(opts)
 				url = "api/v2/interactions"
 				get_object(:post, "interaction", url, {:interaction => opts})
 			end
 
+			# Update a specific interaction for your campaign
 			def update(id, opts)
 				url = "api/v2/interactions/#{id}"
 				get_object(:put, "interaction", url, {:interaction => opts})
@@ -27,6 +43,7 @@ module Playapi
 				get_object(:delete, "interaction", url)
 			end
 
+			# Create a specific interaction for your campaign
 			def classed_interaction(type, opts)
 				url = "api/v2/interactions"
 				validator = "Playapi::Validation::#{type.capitalize}".split("::").inject(Module) {|acc, val| acc.const_get(val)}
