b39a942c17673efdf2e55f8870854b413aaed09b
Christopher France <hipsterelitist@gmail.com>
2013-04-30 13:02:06 -0400

    add custom validation

---
+++ lib/playapi/validation/custom.rb
@@ -0,0 +1,31 @@
+module Playapi
+  module Validation
+    module Custom
+
+    	class << self
+
+        def pkeys
+          @pkeys ||= [
+            :name
+          ]
+        end
+
+        def validate(opts)
+          pkeys.each do |p|
+            unless opts.include?(p)
+              raise "#{p} must not be nil"
+            end
+          end
+          opts.each do |interaction_field, value|
+            #next if value.nil?
+
+            unless value.is_a?(String) || value.is_a?(Integer) || value.is_a?(Float)
+              raise("Invalid #{interaction_field} specified: #{value} must be a string or a number") unless value == points
+            end
+          end
+        end
+    	end
+      
+    end
+  end
+end
