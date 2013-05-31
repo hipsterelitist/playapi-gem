3d6a58eed77da7d3a0aff7988ee05245a86ced20
Christopher France <hipsterelitist@gmail.com>
2013-05-31 13:48:01 -0400

    push snaps

--- lib/playapi/snapchatter.rb
+++ lib/playapi/snapchatter.rb
@@ -2,5 +2,10 @@ module Playapi
 	class Snapchatter < Playapi::Feature
 		attr_reader :username, :password
 
+		def send_snapchat(names=[], url='', opts={})
+			# awaiting finalization of end point
+			url = "/api/v2/snapchatter/send_snapchat"
+			self.class.send(:get_objects, :post, "results", opts.merge!(usernames: names, source: url))
+		end
 	end
 end
